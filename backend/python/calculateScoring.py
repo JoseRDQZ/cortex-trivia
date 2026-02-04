a68e1a0fb4ea400b2c3409c9e27f10e9be0784b9
Renier Herba <renierhb@gmail.com>
2026-02-03 20:57:57 -0500

    Adjusted functions to save in the db folder under results.json

--- backend/python/calculateScoring.py
+++ backend/python/calculateScoring.py
@@ -1,45 +1,44 @@
 import json
+import os
 
+current_folder = os.path.dirname(__file__)
+json_path = os.path.join(current_folder, '..', '..', 'db', 'results.json')
 
-def fetch_question(question_id):
-    db = json.loads(open('db.json').read())
-
-    for question in db:
-        if question['id'] == question_id:
-            return question
-
-    return None
-
-
-def calculate_scoring(question_id, answer_id):
-    """Calculates the scoring of the game session"""
-    question = fetch_question(question_id)
-    if question["answerIndex"] == answer_id:
-        return 1
-    else:
-        return 0
-
-
+# Function needs to be called with player and score parameters
+# If player doesn't have a score it initiates at 0
+# The function does not rewrite values for a new session
+# TODO: Add section_id to the results or initiate results.json at the start of the game
 def save_score(player, score):
-    results = json.loads(open('results.json').read())
+
+    try:
+        with open(json_path, 'r') as file:
+            results = json.load(file)
+    except:
+        results = {}
 
     result = results.get(player, {"score": 0, "total": 0})
-
-    result["score"] = result["score"] + score
+    result["score"] = result["score"] + int(score)
     result["total"] = result["total"] + 1
-
     results[player] = result
 
-    json.dump(results, open('results.json', 'w'))
+    with open(json_path, 'w') as file:
+        return json.dump(results, file)
 
+
+# Gives percentage value from results from total
 def calculate_final_score(player):
     """Calculates the scoring of the game session"""
-    results = json.loads(open('results.json').read())
+    with open(json_path, 'r') as file:
+        results = json.load(file)
     result = results.get(player)
 
     return result["score"] * 100 / result["total"]
 
-
-save_score("OHB", calculate_scoring("E004", 3))
-save_score("OHB", calculate_scoring("E003", 1))
+#This is a test The amount a player scores does not affect the other players /see results.json/
+save_score("OHB", True)
+save_score("OHB", True)
+save_score("OHB", False)
+save_score("OHB", True)
+save_score("AHB", False)
 print(calculate_final_score("OHB"))
+#still haven't made a way to clear results.json, could be with a start game
